7ee63cd07313e76bdfa6265ea5afbe30923cfbd8
Rose Koenings <rnkings.05@gmail.com>
2016-10-18 17:19:09 -0500

    jquery function

--- js/itunes.js
+++ js/itunes.js
@@ -1,2 +1,33 @@
-use strict;
+
+// 
+// GET TUNE
+// 
+
+
+// $('document').ready(function(){
+// 	var tuneName = "Upside Down";   
+	//.value like most wanted for get info?
+
+var tuneNameInput = $('#buttonTune').val();  //is this even right?
+$(selector).val(function(index,currentvalue))
+window.print();
+
+
+function getTune(userInputTuneName){
+	$.ajax({
+		url: "https://itunes.apple.com/search?term=/"+userInputTuneName,
+		type: "GET",
+		dataType: "JSONP"
+	}).done(function(data){
+		console.log(data);
+	}).fail(function(error){
+		console.log("an error occured!");
+	});
+}
+
+
+$("#buttonTune").click(function(){
+	getTune(userInputTuneName);
+});
+
 

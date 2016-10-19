11cac66292123352ff3035ffbb20fc9851eca8f0
Rose Koenings <rnkings.05@gmail.com>
2016-10-19 00:21:48 -0500

    added sound bit and picture

--- js/itunes.js
+++ js/itunes.js
@@ -21,7 +21,9 @@ function printToPage(data){
 		var result = data.results[i];
 		console.log(result.trackName);
 		console.log(result);
-		document.getElementById("printArea").innerHTML += "<hr>" + " Tune Name: " + result.trackName + "<br>" + " Artist: " + result.artistName + "<br>" + " Albumn: " + result.collectionName;
+		var html = "<hr>" + " Tune Name: " + result.trackName + "<br>" + " Artist: " + result.artistName + "<br>" + " Albumn: " + result.collectionName + "<br>" + "<br>"
+		html += "<audio controls><source src='" + result.previewUrl + "' type='audio/mpeg'></audio>" + "<br>" + "<br>" + "<img src='" + result.artworkUrl100 + "' height='120' width='120'>";
+		document.getElementById("printArea").innerHTML += html; 
 	}
 }
 
